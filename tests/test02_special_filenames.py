d1fa1e7327224cb8f505a3bec8021b2ff58f02f4
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2013-02-16 00:52:58 +0800

    * 修正 python3 之特殊檔名問題

--- tests/test02_special_filenames.py
+++ tests/test02_special_filenames.py
@@ -4,24 +4,13 @@
 # Copyright: Chun-Yu Lee (Mat) <matlinuxer2@gmail.com> || Dieter Hsu <dieterplex@gmail.com>
 #
 
+import os
 import vcseedftp
-import codecs
 
-sample_output = [ 
-"""
-D	shopadmin/images/big5/wi0114-48+.gif
-D	templates/v49-1/images/bu t4.jpg
-D	templates/v49-1/images/q 2.gif
-D	templates/v49-1/images/q 22.gif
-D	templates/v49-1/images/title- liulan.gif
-D	"templates/v49-1/images/\\245\\274\\251R\\246W--1_09.jpg"
-D	"templates/v49-1/images/\\245\\274\\251R\\246W--1_14.gif"
-D	"templates/v49-1/images/\\245\\274\\251R\\246W--1_18.gif"
-D	"templates/v49-1/images/\\251\\361\\244J\\301\\312\\252\\253\\250\\256.gif"
-D	"templates/v49-1/images/\\251\\361\\244J\\301\\312\\252\\253\\250\\256.jpg"
-"""
-]
+vcseedftp.LCD = os.path.dirname( __file__ )
 
-result = vcseedftp.convert_diff_to_ftp_commands( sample_output[0] )
+_, sample_output, _ = vcseedftp.popen( 'cat test02_data/diff_output.txt' )
+
+result = vcseedftp.convert_diff_to_ftp_commands( sample_output )
 
 print( result )
